88112f8e547450600bec08d79ba3e642a5dbdacc
Surbhi Agrawal <111246726+SurbhiAgrawal9@users.noreply.github.com>
2023-09-15 18:59:01 +0530

    Create fruit.js

---
+++ javascript-Practical/switch case /fruit.js
@@ -0,0 +1,13 @@
+
+let fruit = "orange";
+
+switch(fruit){
+    case "mango":
+        console.log("the fruit is king");
+        break;
+        case "orange":
+            console.log("the fruit is big");
+            break;
+            default:
+            console.log("please enter the valid input");
+}
