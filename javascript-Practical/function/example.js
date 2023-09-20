eb4adfab5200183b73d017b82f0b5baceec1fe0f
Surbhi Agrawal <111246726+SurbhiAgrawal9@users.noreply.github.com>
2023-09-21 02:30:09 +0530

    Create example.js

---
+++ javascript-Practical/function/example.js
@@ -0,0 +1,11 @@
+Functions in JavaScript
+Functions are the basic building block of JavaScript. Functions allow us to encapsulate a block of code and reuse it multiple times.
+
+Functions make JavaScript code more readable, organized, reusable, and maintainable.
+
+  ```js
+  function <function-name>(arg1, arg2, arg3,...)
+{
+    //write function code here
+};
+```
